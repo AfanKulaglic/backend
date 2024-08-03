144f760d9b5d7a9f8d16d99b82b075252193e727
AfanKulaglic <afankulaglic@outlook.com>
2024-08-03 02:59:57 +0200

    Finish Home page and Category page

--- index.js
+++ index.js
@@ -40,9 +40,42 @@ app.post('/api/data', async (req, res) => {
 app.get('/api/data', async (req, res) => {
     try {
         const data = await Data.find();
+        if (data.length === 0) {
+            const defaultData = [
+                { field1: 'Default Field 1 - 1', field2: 'Default Field 2 - 1' },
+                { field1: 'Default Field 1 - 2', field2: 'Default Field 2 - 2' },
+            ];
+            return res.status(200).send(defaultData);
+        }
         res.status(200).send(data);
     } catch (error) {
         res.status(500).send(error);
+    }
+});
+
+app.delete('/api/data/:id', async (req, res) => {
+    const { id } = req.params;
+
+    console.log(`Received request to delete item with ID: ${id}`);
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        console.error(`Invalid ObjectId format: ${id}`);
+        return res.status(400).send({ message: 'Invalid ID format' });
+    }
+
+    try {
+        const deletedData = await Data.findByIdAndDelete(id);
+
+        if (!deletedData) {
+            console.error(`Item with ID: ${id} not found`);
+            return res.status(404).send({ message: 'Data not found' });
+        }
+
+        console.log(`Item with ID: ${id} successfully deleted`);
+        return res.status(200).send(deletedData);
+    } catch (error) {
+        console.error('Error deleting data:', error);
+        return res.status(500).send({ message: 'Internal server error' });
     }
 });
 
