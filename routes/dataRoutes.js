5d196fa15d33b93bb8dfb54c8e5b09140e68f281
AfanKulaglic <afankulaglic@outlook.com>
2024-08-18 21:31:56 +0200

    Initial commit

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -60,41 +60,51 @@ router.patch('/data/:id/messages', async (req, res) => {
     const { id } = req.params;
     const { user, content, toUser, _id, timestamp } = req.body;
 
+    // Proverite da li je ID u ispravnom formatu
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).send({ message: 'Invalid ID format' });
     }
+
+    // Proverite da li svi potrebni podaci postoje
     if (!user || !content || !toUser || !_id || !timestamp) {
         return res.status(400).send({ message: 'User, content, recipient user, ID, or timestamp is missing' });
     }
 
     try {
+        // Pronađi prijatelja po ID-u
         const updatedFriendData = await Data.findById(id);
         if (!updatedFriendData) {
             return res.status(404).send({ message: 'Friend data not found' });
         }
 
+        // Proveri da li poruka već postoji za prijatelja
         const messageExistsForFriend = updatedFriendData.messages.some(m => m._id === _id);
         if (!messageExistsForFriend) {
             updatedFriendData.messages.push({ user, content, toUser, _id, timestamp });
             await updatedFriendData.save();
         }
 
+        // Pronađi korisnika koji je poslao poruku
         const userData = await Data.findOne({ nickname: user });
         if (userData) {
+            // Proveri da li poruka već postoji za korisnika
             const messageExistsForUser = userData.messages.some(m => m._id === _id);
             if (!messageExistsForUser) {
-                // Zadrži originalnu vrednost toUser umesto zamenjivanja sa userData.nickname
                 userData.messages.push({ user, content, toUser, _id, timestamp });
                 await userData.save();
             }
         }
 
-        io.emit('newMessage', { friendData: updatedFriendData, userData }); // Emitovanje događaja
+        // Emituj događaj u realnom vremenu
+        io.emit('receiveMessage', { friendData: updatedFriendData, userData }); 
+
         res.status(200).send({ friendData: updatedFriendData, userData });
     } catch (error) {
+        console.error('Error updating data with message:', error.message);
         res.status(500).send({ message: 'Error updating data with message', error });
     }
 });
+
 
 
 router.delete('/data/:id', async (req, res) => {
